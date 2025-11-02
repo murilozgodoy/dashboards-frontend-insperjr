50844bd3980e860c768cf69b59e7f848af9fea02
Enzo Perego <enzorp@al.insper.edu.br>
2025-11-02 19:54:49 -0300

    Botoes Filtro dos Graficos

--- src/components/VolumeHoraPlataformasChart.tsx
+++ src/components/VolumeHoraPlataformasChart.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import styled from 'styled-components';
+import { CHART_COLORS } from '../config/colors';
 
 interface DadosHora {
   hora: number;
@@ -175,8 +176,8 @@ const ToggleGroup = styled.div`
 
 const ToggleButton = styled.button<{ $active: boolean }>`
   padding: 0.375rem 0.75rem;
-  border: 1px solid ${props => props.$active ? '#792810' : '#e2e8f0'};
-  background-color: ${props => props.$active ? '#792810' : 'white'};
+  border: 1px solid ${props => props.$active ? CHART_COLORS.marrom : '#e2e8f0'};
+  background-color: ${props => props.$active ? CHART_COLORS.marrom : 'white'};
   color: ${props => props.$active ? 'white' : '#4a5568'};
   border-radius: 6px;
   font-size: 0.875rem;
@@ -185,8 +186,8 @@ const ToggleButton = styled.button<{ $active: boolean }>`
   transition: all 0.2s;
   
   &:hover {
-    border-color: #792810;
-    background-color: ${props => props.$active ? '#5C1F0C' : '#f7f1ef'};
+    border-color: ${CHART_COLORS.marrom};
+    background-color: ${props => props.$active ? '#78350f' : '#f7f1ef'};
   }
 `;
 
